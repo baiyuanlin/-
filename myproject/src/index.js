ceadddc39fd2e46a815d4a078b7180597003e330
hongen <274095759@qq.com>
2018-07-08 14:37:19 +0800

    hongen

--- myproject/src/index.js
+++ myproject/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Find from './component/find';
+import App from './component/app';
+
+import {Router,Route,hashHistory,IndexRoute,IndexRedirect,Redirect} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
