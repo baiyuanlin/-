22172092e47b19a483184e2d82033d2466e2b1d6
haojie <782319624@qq.com>
2018-07-08 14:39:00 +0800

    王

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Special from './special';
+import Spbieshu from './spbieshu';
+import Spsearch from './spsearch';
+import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+					    <Switch>
+							<Route path="/special" component={Special}></Route>
+							<Route path="/spsearch" component={Spsearch}></Route>
+							<Route path="/spbieshu" component={Spbieshu}></Route>
+							<Redirect to="/special"/>
+							</Switch>
+				</Router>
     );
   }
 }
